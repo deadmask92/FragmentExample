c20e98d06e4b046abc7f5a59da69e512a1f5b24c
mac-max <luca.anghileri@ff3d.com>
2014-11-11 14:44:36 +0100

    Last Changes

--- app/src/main/java/com/ff3d/fragmentexample/MainActivity.java
+++ app/src/main/java/com/ff3d/fragmentexample/MainActivity.java
@@ -1,17 +1,32 @@
 package com.ff3d.fragmentexample;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements Frag1.OnFragmentInteractionListener {
+
+    Frag1 frag1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frag1 = Frag1.newInstance("11-11-2014 12:28:00");
+
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.container, frag1);
+        fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commit();
+
     }
 
 
@@ -35,5 +50,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFrag1Interaction(){
+
+    }
+
+    public void Update(View view) {
+        frag1.UpdateDateTime2(frag1.getView(), "11-11-2014 13:00:00");
     }
 }
